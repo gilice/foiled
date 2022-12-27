96c02327e2516a4d856a1220f285992d1106da11
gilice <104317939+gilice@users.noreply.github.com>
2022-12-27 20:53:44 +0100

    Source refactor 2

---
+++ lib/shared/ui/info_card.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'info_card.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class InfoCard extends StatelessWidget {
+  const InfoCard({
+    Key? key,
+    this.child,
+    this.message,
+  }) : super(key: key);
+
+  final Widget? child;
+
+  final String? message;
+
+  @override
+  Widget build(BuildContext _context) => infoCard(
+        _context,
+        child: child,
+        message: message,
+      );
+}
